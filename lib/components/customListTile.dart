870985deef4ee3144c145a232faa728167f5a437
Rahil Ansari <workbyrahil@gmail.com>
2021-11-26 20:55:11 +0530

    first commit

---
+++ lib/components/customListTile.dart
@@ -0,0 +1,76 @@
+import 'package:news_application/model/article_model.dart';
+import 'package:news_application/pages/articles_details_page.dart';
+import 'package:flutter/material.dart';
+
+Widget customListTile(Article article, BuildContext context) {
+  return InkWell(
+    onTap: () {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => ArticlePage(
+                    article: article,
+                  )));
+    },
+    child: Container(
+      margin: EdgeInsets.all(12.0),
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 3.0,
+            ),
+          ]),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 200.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              //let's add the height
+
+              image: DecorationImage(
+                  image: NetworkImage(article.urlToImage), fit: BoxFit.cover),
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Container(
+            padding: EdgeInsets.all(6.0),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            child: Text(
+              article.source.name,
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            article.title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16.0,
+            ),
+          )
+        ],
+      ),
+    ),
+  );
+}
+
+// ignore: non_constant_identifier_names
+ArticlePage({required Article article}) {
+}
---
+++ lib/services/api_service.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:news_application/model/article_model.dart';
+import 'package:http/http.dart';
+
+
+
+class ApiService {
+  final endPointUrl =
+      "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=API_KEY";
+
+  
+
+  Future<List<Article>> getArticle() async {
+    Response res = await get(Uri.parse(endPointUrl));
+
+    if (res.statusCode == 200) {
+      Map<String, dynamic> json = jsonDecode(res.body);
+
+      List<dynamic> body = json['articles'];
+
+      List<Article> articles =
+          body.map((dynamic item) => Article.fromJson(item)).toList();
+
+      return articles;
+    } else {
+      throw ("Can't get the Articles");
+    }
+  }
+}
